f7a97fd125dd54854497b56fe211fec32f297b4d
montsecaabello <montsecabello2705@gmail.com>
2026-07-24 17:11:45 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Gol Shirt",
   description: "Catálogo de camisetas de fútbol",
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({
